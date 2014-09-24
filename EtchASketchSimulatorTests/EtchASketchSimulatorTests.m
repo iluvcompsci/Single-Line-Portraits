e1a324ef837b7890d30014d04dd57bf1ceddee51
Briana Chapman <bri.chapman@gmail.com>
2014-09-24 13:25:36 -0500

    halloooo committed finally go bri.

---
+++ EtchASketchSimulatorTests/EtchASketchSimulatorTests.m
@@ -0,0 +1,34 @@
+//
+//  EtchASketchSimulatorTests.m
+//  EtchASketchSimulatorTests
+//
+//  Created by Bri Chapman on 9/15/14.
+//  Copyright (c) 2014 edu.illinois.bchapman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EtchASketchSimulatorTests : XCTestCase
+
+@end
+
+@implementation EtchASketchSimulatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
